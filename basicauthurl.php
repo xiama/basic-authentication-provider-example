78f597a00c948dd31bb602f1c197e2b7253c9bbb
Xiaoqiang Ma <xiama@redhat.com>
2015-08-20 11:03:49 +0800

    Update basicauthurl.php

--- basicauthurl.php
+++ basicauthurl.php
@@ -3,7 +3,12 @@
 header('Content-Type: application/json');
 $user = $_SERVER['REMOTE_USER'];
 $n=strpos($user,'@');
-if ($n) $str=substr($user,0,$n);
+if ($n) {
+  $str=strtolower(substr($user,0,$n));
+}
+else {
+  $str=strtolower($user)
+}
 $data = array( 'sub' => $str );
 echo json_encode($data);
 
